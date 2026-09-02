1fea7ec42e352aebf5a252003c90aed260297019
YUTIAN cao <804860914@qq.com>
2026-09-02 15:36:56 +0800

    添加MySQL依赖和模块骨架，整理gitignore

---
+++ MainModule/src/main/java/AppMain.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class AppMain
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(
+                "Hello,world"
+        );
+    }
+}
